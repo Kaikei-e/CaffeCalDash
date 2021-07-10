adf383c6f1e7c53c5285248da32f5e69dad1e841
e_kaikei <e_kaikei@protonmail.com>
2021-07-10 15:26:40 +0900

    Rebuild.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(CaffeCalDash());
+}
+
+class CaffeCalDash extends StatefulWidget{
+  CaffeCalDash({Key? key}) : super(key: key);
+
+  @override
+  _CaffeCalDash createState() => _CaffeCalDash();
+}
+
+class _CaffeCalDash extends State<CaffeCalDash>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
